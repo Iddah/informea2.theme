0bc2ab421198abeec82771f27dd62edd5137b4a2
Cristian Romanescu <cristian@romanescu.ro>
2013-03-27 16:39:00 +0200

    Added featured treaty portlet

--- functions.php
+++ functions.php
@@ -16,10 +16,11 @@ function informea_is_staging() {
     return get_bloginfo('url') == 'http://test.informea.org';
 }
 
-function informea2_primary_nav_menu_args( $args ) {
+function informea2_primary_nav_menu_args($args) {
     $args['show_home'] = FALSE;
     return $args;
 }
+
 add_filter('wp_nav_menu_args', 'informea2_primary_nav_menu_args');
 
 
@@ -27,24 +28,26 @@ function informea2_setup() {
     register_nav_menus(array(
         'primary' => __('Primary Navigation', 'twentyten'),
     ));
-
 }
+
 add_action('after_setup_theme', 'informea2_setup');
 
 
 function informea2_widgets_init() {
-register_sidebar(array(
-    'name' => __('Index page first column', 'informea'),
-    'id' => 'index-page-col1',
-    'description' => __('Index page first column widgets', 'informea'),
-    'before_widget' => '<div class="portlet">',
-    'after_widget' => '</div>',
-    'before_title' => '<div class="pre-title"><div class="title"><span>',
-    'after_title' => '</span></div></div>',
-));
+    register_sidebar(array(
+        'name' => __('Index page first column', 'informea'),
+        'id' => 'index-page-col1',
+        'description' => __('Index page first column widgets', 'informea'),
+        'before_widget' => '<div class="portlet">',
+        'after_widget' => '</div>',
+        'before_title' => '<div class="pre-title"><div class="title"><span>',
+        'after_title' => '</span></div></div>',
+    ));
 }
+
 add_action('widgets_init', 'informea2_widgets_init');
 
 
 require_once(dirname(__FILE__) . '/widgets/collapsible-text.php');
 require_once(dirname(__FILE__) . '/widgets/latest-news.php');
+require_once(dirname(__FILE__) . '/widgets/featured-treaty.php.php');
---
+++ widgets/featured-treaty.php
@@ -0,0 +1,107 @@
+<?php
+class FeaturedTreatyWidget extends WP_Widget {
+
+    function FeaturedTreatyWidget() {
+        $options = array(
+            'classname' => 'FeaturedTreatyWidget',
+            'description' => 'Rotate one of the treaties daily',
+        );
+        $this->WP_Widget('FeaturedTreatyWidget', 'Featured Treaty', $options);
+    }
+
+
+    function form($instance) {
+        $instance = wp_parse_args((array)$instance, array('title' => '', 'count_decisions' => FALSE, 'count_nfp' => FALSE));
+        $title = $instance['title'];
+        $count_decisions = $instance['count_decisions'];
+        $count_nfp = $instance['count_nfp'];
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id('title'); ?>">Title:</label>
+            <input class="widefat"
+                   id="<?php echo $this->get_field_id('title'); ?>"
+                   name="<?php echo $this->get_field_name('title'); ?>" type="text"
+                   value="<?php echo esc_attr($title); ?>"
+                />
+        </p>
+        <p>
+            <input type="checkbox"
+                   id="<?php echo $this->get_field_id('count_decisions'); ?>"
+                   name="<?php echo $this->get_field_name('count_decisions'); ?>" value="1"
+                <?php echo $count_decisions == 1 ? 'checked="checked"' : ''; ?>
+                />
+            <label for="<?php echo $this->get_field_id('count_decisions'); ?>">Show decisions count (if
+                available)</label>
+        </p>
+        <p>
+            <input type="checkbox"
+                   id="<?php echo $this->get_field_id('count_nfp'); ?>"
+                   name="<?php echo $this->get_field_name('count_nfp'); ?>" value="1"
+                <?php echo $count_nfp == 1 ? 'checked="checked"' : ''; ?>
+                />
+            <label for="<?php echo $this->get_field_id('count_nfp'); ?>">Show NFP count (if available)</label>
+        </p>
+    <?php
+    }
+
+    function update($new_instance, $old_instance) {
+        $instance = $old_instance;
+        $instance['title'] = $new_instance['title'];
+        $instance['count_decisions'] = $new_instance['count_decisions'];
+        $instance['count_nfp'] = $new_instance['count_nfp'];
+        return $instance;
+    }
+
+    function widget($args, $instance) {
+        $title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+        $count_decisions = $instance['count_decisions'] == 1;
+        $count_nfp = $instance['count_nfp'] == 1;
+        $treaty = informea_treaties::get_featured_treaty(1);
+        if ($treaty) {
+            ?>
+            <div class="portlet">
+                <?php if (!empty($title)) : ?>
+                    <div class="title">
+                        <?php echo $title; ?>
+                    </div>
+                <?php endif; ?>
+                <div class="content featured-treaty">
+                    <img src="<?php echo $treaty->logo_medium; ?>"/>
+                    <a href="<?php echo sprintf('%s/treaties/%s', get_bloginfo('url'), $treaty->odata_name); ?>">
+                        <?php echo $treaty->short_title; ?>
+                    </a>
+                    <?php
+                    if ($count_decisions) :
+                        $ob = new informea_treaties();
+                        $c = $ob->get_decisions_count_2($treaty->id);
+                        if ($c > 0) :
+                            ?>
+                            <br/>
+                            Decisions: <a
+                            href="<?php echo sprintf('%s/treaties/%s/decisions', get_bloginfo('url'), $treaty->odata_name); ?>"><?php echo $c; ?></a>
+                        <?php endif; ?>
+                    <?php endif; ?>
+                    <?php
+                    if ($count_nfp) :
+                        $c = informea_treaties::treaty_count_nfp($treaty->id);
+                        if ($c > 0) :
+                            ?>
+                            <br/>
+                            Focal points: <a
+                            href="<?php echo sprintf('%s/treaties/%s/nfp', get_bloginfo('url'), $treaty->odata_name); ?>"><?php echo $c; ?></a>
+                        <?php endif; ?>
+                    <?php endif; ?>
+                    <?php if (!empty($treaty->region)) :
+                        echo '<br />Region: ' . $treaty->region;
+                    endif; ?>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div class="clear"></div>
+            <div class="margin-bottom-10"></div>
+        <?php
+        }
+    }
+}
+
+add_action('widgets_init', create_function('', 'return register_widget("FeaturedTreatyWidget");'));?>
