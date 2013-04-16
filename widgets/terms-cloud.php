dab8fafa3a026d67731af45324491dd03ff34cd5
Cristian Romanescu <cristian@romanescu.ro>
2013-04-16 13:50:49 +0300

    Added terms cloud widget

---
+++ widgets/terms-cloud.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Author: Cristian Romanescu <cristi _at_ eaudeweb dot ro>
+ * Created: 201304161328
+ */
+
+class TermCloudWidget extends WP_Widget {
+
+    function TermCloudWidget() {
+        $options = array(
+            'classname' => 'TermCloudWidget',
+            'description' => 'Show relevant InforMEA terms in context of the displayed data',
+        );
+        $this->WP_Widget('TermCloudWidget', 'InforMEA terms cloud', $options);
+    }
+
+
+    function form($instance) {
+        $instance = wp_parse_args((array)$instance, array('title' => ''));
+        $title = $instance['title'];
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id('title'); ?>">Title:</label>
+            <input class="widefat"
+                   id="<?php echo $this->get_field_id('title'); ?>"
+                   name="<?php echo $this->get_field_name('title'); ?>" type="text"
+                   value="<?php echo esc_attr($title); ?>"
+                />
+        </p>
+    <?php
+    }
+
+    function update($new_instance, $old_instance) {
+        $instance = $old_instance;
+        $instance['title'] = $new_instance['title'];
+        return $instance;
+    }
+
+    function widget($args, $instance) {
+        global $cloud_terms;
+        $title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+        if(!empty($cloud_terms)) {
+            $terms = $cloud_terms;
+        } else {
+            $terms = Thesaurus::get_top_concepts();
+        }
+        $terms = imea_page_base_page::compute_popularity($terms);
+    ?>
+            <div class="portlet featured-country">
+                <?php if (!empty($title)) : ?>
+                    <div class="title">
+                        <?php echo $title; ?>:
+                    </div>
+                <?php endif; ?>
+                <div class="content terms-cloud tags">
+                    <ul>
+                        <?php foreach($terms as $_term) : ?>
+                        <li>
+                            <a class="tag<?php echo $_term->popularity; ?>"
+                               href="<?php bloginfo('url'); ?>/terms/<?php echo $_term->id; ?>"><?php echo $_term->tag; ?></a>
+                        </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div class="clear"></div>
+            <div class="margin-bottom-10"></div>
+        <?php
+    }
+}
+
+add_action('widgets_init', create_function('', 'return register_widget("TermCloudWidget");'));
