9afa3bdd5699e7d02eb438b7915825bdd5e1adb4
Cristian Romanescu <cristian@romanescu.ro>
2013-04-18 13:02:29 +0300

    Utility for breadcrumbtrails

--- functions.php
+++ functions.php
@@ -1,5 +1,24 @@
 <?php
-// Breadcrumbtrail generation
+function build_breadcrumbtrail($items) {
+    $ret =  '<div id="breadcrumbtrail">';
+    $ret .= '<ul>';
+    $ret .= sprintf('<li>%s</li>', __('You are here:', 'informea'));
+    $c = count($items);
+    $i = 0;
+    foreach($items as $label => $url) {
+        $raquo = $i < $c - 1 ? '<span class="separator">&raquo;</span>' : '';
+        if(!empty($url)) {
+            $ret .= sprintf('<li><a href="%s">%s</a>%s</li>', $url, $label, $raquo);
+        } else {
+            $ret .= sprintf('<li>%s%s</li>', $label, $raquo);
+        }
+        $i++;
+    }
+    $ret .= '</ul>';
+    $ret .= '</div>';
+    return $ret;
+}
+
 function informea_breadcrumbtrail() {
     global $post;
     $ret = '';
