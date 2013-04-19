7a7851efc5a00c09aa5d0061f25ca768fa68881e
Cristian Romanescu <cristian@romanescu.ro>
2013-04-19 13:19:40 +0300

    Left sidebar is dynamically computed

--- pages/left_column/sidebar.php
+++ pages/left_column/sidebar.php
@@ -1,1 +1,8 @@
+<?php
+global $post;
+$post_name = $post->post_name;
+$id = get_request_variable('id');
+$sidebar_name = $post_name . '-sidebar' . (empty($id) ? '' : '-item');
+?>
 <div class="clear">&nbsp;</div>
+<?php dynamic_sidebar($sidebar_name); ?>
