9a25848ab2446a8ffb9e9322dc6780da6e6b9304
Cristian Romanescu <cristian@romanescu.ro>
2013-04-24 15:07:23 +0300

    Added grid view

--- pages/content/page-countries-grid.php
+++ pages/content/page-countries-grid.php
@@ -1,1 +1,52 @@
-xx
+<?php
+do_action('informea-countries-toolbar');
+$data = informea_countries::index_grid();
+$columns = $data['column'];
+$signatures = $data['signatures'];
+$countries = $data['countries'];
+?>
+<div class="table-scroll-wrapper">
+<div class="table-scroll">
+<table id="table-grid">
+    <thead>
+    <tr>
+        <th>&nbsp;</th>
+        <th><?php _e('Country', 'informea') ?></th>
+        <?php foreach ($columns as $column) { ?>
+            <th><?php echo $column->short_title; ?></th>
+        <?php } ?>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($countries as $count => $country) : ?>
+        <tr>
+            <td>
+                <a href="<?php echo get_permalink() . '/' . $country->id ?>">
+                    <img src="<?php bloginfo('template_directory'); ?>/<?php echo $country->icon_medium; ?>" alt="" />
+                </a>
+            </td>
+            <td>
+                <a href="<?php echo get_permalink() . '/' . $country->id ?>">
+                    <?php echo $country->name; ?>
+                </a>
+            </td>
+            <?php
+            foreach ($columns as $column) :
+                $id_treaty = $column->id;
+                $id_country = $country->id;
+                $coldata = '-';
+                if (isset($signatures[$id_treaty])) {
+                    $tmparr = $signatures[$id_treaty];
+                    if (isset($tmparr[$id_country])) {
+                        $coldata = $tmparr[$id_country];
+                    }
+                }
+                ?>
+                <td><?php echo $coldata; ?></td>
+            <?php endforeach; ?>
+            </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+</div>
+</div>
