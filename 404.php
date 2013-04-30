c1a46906c2bcb8da8d4e1deb3f10f32d89297647
Cristian Romanescu <cristian@romanescu.ro>
2013-04-30 16:14:06 +0300

    Added 404 page

--- 404.php
+++ 404.php
@@ -1,1 +1,22 @@
-404
+<?php
+get_header();
+?>
+<div class="col2-left col2">
+    &nbsp;
+</div>
+<div class="col2-center col2">
+    <h1>Page gone missing</h1>
+    <div class="d404 round">
+        <p>
+            While this is embarassing, we've recently updated the website and some things may have broken.
+            <br />
+            Please visit our <a href="<?php bloginfo('url'); ?>">home page</a> and start fresh.
+        </p>
+        <p>
+            We'll do better next time!
+        </p>
+    </div>
+    <img src="http://www.informea.org/wp-content/uploads/2013/04/robot.jpg" />
+</div>
+<?php
+get_footer();
