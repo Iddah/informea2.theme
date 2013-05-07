9c4b3bc590c7d9f7c4d4668faec16c3449c98373
Cristian Romanescu <cristian@romanescu.ro>
2013-05-07 10:37:30 +0300

    Focus the search box on index page

--- scripts/index.js
+++ scripts/index.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
     /* Set-up paginator for the upcoming meetings widget */
     upcoming_meetings_paginator();
 
+    $('#q_freetext').focus();
     $("#q_term_index").reusableComboBox({
         select: function (evt, ob) {
             var item = ob.item;
