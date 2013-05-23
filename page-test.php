1842ab14c1d2e0ac2aac96cf0fd5ec7a44122e0f
Cristian Romanescu <cristian@romanescu.ro>
2013-05-23 13:48:01 +0300

    Fixed all logo styles in sprite & css

---
+++ page-test.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+
+get_header(); ?>
+<div id="container" style="width: 100% !important;">
+    <div id="content" role="main">
+        <div class="cbd pull-left thumbnail"></div>
+        <div class="cartagena pull-left thumbnail"></div>
+        <div class="nagoya pull-left thumbnail"></div>
+        <div class="cms pull-left thumbnail"></div>
+        <div class="cites pull-left thumbnail"></div>
+        <div class="plant-treaty pull-left thumbnail"></div>
+        <div class="ramsar pull-left thumbnail"></div>
+        <div class="whc pull-left thumbnail"></div>
+        <div class="basel pull-left thumbnail"></div>
+        <div class="rotterdam pull-left thumbnail"></div>
+        <div class="stockholm pull-left thumbnail"></div>
+        <div class="unccd pull-left thumbnail"></div>
+        <div class="unfccc pull-left thumbnail"></div>
+        <div class="kyoto pull-left thumbnail"></div>
+        <div class="vienna pull-left thumbnail"></div>
+        <div class="montreal pull-left thumbnail"></div>
+
+        <div class="clear"></div>
+        <div class="abidjan pull-left thumbnail"></div>
+        <div class="nairobi pull-left thumbnail"></div>
+        <div class="bamako pull-left thumbnail"></div>
+        <div class="lusakaagreement pull-left thumbnail"></div>
+
+        <div class="clear"></div>
+        <div class="noumea pull-left thumbnail"></div>
+        <div class="apia pull-left thumbnail"></div>
+
+        <div class="clear"></div>
+        <div class="barcelona pull-left thumbnail"></div>
+        <div class="dumping pull-left thumbnail"></div>
+        <div class="barc-spa pull-left thumbnail"></div>
+        <div class="preventionemergency pull-left thumbnail"></div>
+        <div class="offshore pull-left thumbnail"></div>
+        <div class="land-based pull-left thumbnail"></div>
+        <div class="hazardous pull-left thumbnail"></div>
+
+        <div class="clear"></div>
+        <div class="aarhus pull-left thumbnail"></div>
+        <div class="espoo pull-left thumbnail"></div>
+        <div class="lrtap pull-left thumbnail"></div>
+        <div class="kyivsea pull-left thumbnail"></div>
+        <div class="protocolwaterhealth pull-left thumbnail"></div>
+        <div class="waterconvention pull-left thumbnail"></div>
+        <div class="pollutantrelease pull-left thumbnail"></div>
+        <div class="industrialaccidents pull-left thumbnail"></div>
+
+        <div class="clear"></div>
+        <div class="aewa pull-left thumbnail"></div>
+
+        <div class="clear"></div>
+        <div class="cartagena-conv pull-left thumbnail"></div>
+        <div class="antigua pull-left thumbnail"></div>
+
+        <div class="clear"></div>
+        <div class="jeddah pull-left thumbnail"></div>
+        <div class="kuwait pull-left thumbnail"></div>
+    </div>
+    <!-- #content -->
+</div><!-- #container -->
+<?php get_footer(); ?>
