bad87107565a29a46a233504cb2b2c4ea64a3c3f
Cristian Romanescu <cristian@romanescu.ro>
2013-05-24 18:58:22 +0300

    [#192] Added official country name

--- pages/titles/title-countries-item.php
+++ pages/titles/title-countries-item.php
@@ -5,4 +5,5 @@ $country = informea_countries::get_country_for_id($id);
 <div id="page-title">
     <img src="<?php bloginfo('template_directory'); ?>/<?php echo $country->icon_medium; ?>" alt="" />
     <h1><?php echo $country->name; ?></h1>
+    <p><?php echo $country->long_name; ?></p>
 </div>
