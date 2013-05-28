74c88d332e2d85416180f875798cf1a3704bc0f2
Cristian Romanescu <cristian@romanescu.ro>
2013-05-28 15:18:31 +0300

    Added the definitions of terms default visibile. Addefd filter by letter

--- pages/content/page-terms-list.php
+++ pages/content/page-terms-list.php
@@ -1,27 +1,37 @@
 <?php
 $dictionary = Thesaurus::index_alphabetical();
 $letters = Thesaurus::get_alphabet_letters();
-$definitions = get_request_int('definitions');
+$hide_definitions = get_request_int('hide_definitions') == 1;
 
 function informea_terms_toolbar_list() {
-    $definitions = get_request_int('definitions') == 1 ;
-    $checked = $definitions? ' checked="checked"' : '';
+    $hide_definitions = get_request_int('hide_definitions') == 1 ;
+    $checked = $hide_definitions ? ' checked="checked"' : '';
+    $letters = Thesaurus::get_alphabet_letters();
 ?>
-    <form action="<?php echo get_permalink(); ?>/list/" id="show-definitions-toolbar-form">
+    <form action="<?php echo get_permalink(); ?>/list/" id="hide-definitions-toolbar-form">
         <label>
-            <input type="checkbox" name="definitions" value="<?php echo $definitions ? '0' : '1'; ?>" <?php echo $checked; ?>
-                   onclick="$('#show-definitions-toolbar-form').submit();" />
-            Show terms definitions
+            <input type="checkbox" name="hide_definitions" value="1" <?php echo $checked; ?>
+                   onclick="$('#hide-definitions-toolbar-form').submit();" />
+            Hide terms definitions
         </label>
     </form>
+    <div class="separator-h-20">
+        Navigate to letter: 
+    <?php foreach ($letters as $letter): ?>
+        <a href="#letter_<?php echo $letter->letter; ?>"><?php echo $letter->letter; ?></a>
+    <?php endforeach; ?>
+    </div>
 <?php
 }
 add_action('informea-terms-toolbar-extra', 'informea_terms_toolbar_list');
 do_action('informea-terms-toolbar');
 ?>
+
+<?php if(!$hide_definitions) : ?>
 <ul class="terms">
 <?php foreach ($letters as $letter): ?>
     <li>
+        <a name="letter_<?php echo $letter->letter; ?>"></a>
         <h2><?php echo $letter->letter; ?></h2>
         <table class="content table-hover">
             <?php $terms = $dictionary[$letter->letter]; ?>
@@ -30,14 +40,27 @@ do_action('informea-terms-toolbar');
                 <td class="text-top" style="width: 200px;">
                     <a href="<?php echo get_permalink() . $term->id; ?>"><?php echo $term->term;?></a>
                 </td>
-                <?php if($definitions == 1) : ?>
                 <td class="text-top">
                     <?php echo $term->description; ?>
                 </td>
-                <?php endif; ?>
             </tr>
             <?php endforeach; ?>
         </table>
     </li>
 <?php endforeach; ?>
 </ul>
+
+<?php else: ?>
+<ul class="terms inline">
+    <?php foreach ($letters as $letter): ?>
+        <li>
+            <a name="letter_<?php echo $letter->letter; ?>"></a>
+            <h2><?php echo $letter->letter; ?></h2>
+            <?php $terms = $dictionary[$letter->letter]; ?>
+            <?php foreach ($terms as $term): ?>
+            <a href="<?php echo get_permalink() . $term->id; ?>"><?php echo $term->term;?></a>
+            <?php endforeach; ?>
+        </li>
+    <?php endforeach; ?>
+</ul>
+<?php endif; ?>
