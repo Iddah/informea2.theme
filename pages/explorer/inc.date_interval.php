6e132a1e576efe2afc94a34c122e34ec784d51d3
Cristian Romanescu <cristian@romanescu.ro>
2013-05-31 12:14:36 +0300

    [#195]Fixed styles for search page

--- pages/explorer/inc.date_interval.php
+++ pages/explorer/inc.date_interval.php
@@ -1,7 +1,9 @@
 <div class="clear"></div>
 
+Interval between years
+<br />
 <select name="q_start_year">
-    <option value="">Start year</option>
+    <option value="">Start</option>
     <?php
     foreach ($search->ui_compute_years() as $y) {
         $search->ui_write_option($y, $y, $y == $search->ui_get_start_year());
@@ -10,7 +12,7 @@
 </select>
 and
 <select name="q_end_year">
-    <option value="">End year</option>
+    <option value="">End</option>
     <?php
     foreach (array_reverse($search->ui_compute_years()) as $y) {
         $search->ui_write_option($y, $y, $y == $search->ui_get_end_year());
